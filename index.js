21a2d5b72a186f96fd77a80ec99e853eb4285a61
24LP2179 24LP2179 <24LP2179@G303-CC0156.local>
2024-12-23 12:27:31 +0800

    9* 9 iin heddehysve?

---
+++ index.js
@@ -0,0 +1,39 @@
+var day = 10
+
+
+
+if (day >= 1 && day <= 9) {
+    console.log('ehnii 9')
+} else
+    if (day >= 10 && day <= 18) {
+        console.log('2dahi9')
+    } else
+        if (day >= 19 && day <= 27) {
+            console.log('3dahi9')
+        } else
+            if (day >= 28 && day <= 36) {
+                console.log('4dahi9')
+            } else
+                if (day >= 37 && day <= 45) {
+                    console.log('5dahi9')
+                }
+                else
+                    if (day >= 46 && day <= 54) {
+                        console.log('6dahi9')
+                    }
+                    else
+                        if (day >= 55 && day <= 63) {
+                            console.log('7dahi9')
+                        }
+                        else
+                            if (day >= 64 && day <= 72) {
+                                console.log('8dahi9')
+                            }
+                            else
+                                if (day >= 73 && day <= 81) {
+                                    console.log('9dahi9')
+                                }
+                                else
+                                    if(day >= 81) {
+    console.log('dulaahan bolno')
+}
