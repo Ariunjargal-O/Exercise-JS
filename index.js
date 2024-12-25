48884e2a534a903915dd175b38358ef1adb66279
24LP2179 24LP2179 <24LP2179@G303-CC0156.local>
2024-12-25 09:24:36 +0800

    bodlogo

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
-var day = 10
+// var text = '';
+// text = text + '*';
+// console.log(text);
+
+// text = text + '*'
+// console.log(text);
+
+// *
+// **
+// ***
+// ****
+// *****  1t hiih.
+// *****
+// ****
+// ***
+// **
+// *   2t hiih.
+
+// var text = ''
+// for(var i = 0; i <+ 5; i++){
+//     text = text + '*';
+//     console.log(text);
+// }
+
+// var text = ''
+// for(var i = 0; i <+ 5; i++){
+//     text = text + 'a';
+//     console.log(text);
+// }
+
+
+// ----------1-100 hurtelh sondgoi tog bichih
+
+// for(var i = 0; i <=100; i++ ){
+//     if (i % 2 == 1)
+//     console.log(i)
+// }
 
 
 
-if (day >= 1 && day <= 9) {
-    console.log('ehnii 9')
-} else
-    if (day >= 10 && day <= 18) {
-        console.log('2dahi9')
-    } else
-        if (day >= 19 && day <= 27) {
-            console.log('3dahi9')
-        } else
-            if (day >= 28 && day <= 36) {
-                console.log('4dahi9')
-            } else
-                if (day >= 37 && day <= 45) {
-                    console.log('5dahi9')
-                }
-                else
-                    if (day >= 46 && day <= 54) {
-                        console.log('6dahi9')
-                    }
-                    else
-                        if (day >= 55 && day <= 63) {
-                            console.log('7dahi9')
-                        }
-                        else
-                            if (day >= 64 && day <= 72) {
-                                console.log('8dahi9')
-                            }
-                            else
-                                if (day >= 73 && day <= 81) {
-                                    console.log('9dahi9')
-                                }
-                                else
-                                    if(day >= 81) {
-    console.log('dulaahan bolno')
-}
+
+// -------1-100 hurtelh buh toonii niiilber
+
+// var sum = 0;
+// for(var i = 0; i <= 100; i++){
+// sum = sum + i;
+// } console.log(sum)
+
+// -----------1-100 hurtelh toonii urjver
+
+// var mul = 1;
+// for(var i = 1; i <= 100; i++){
+//     mul = mul * i;
+// } console.log(mul)
+
+
+
+// 1-100 -----------hurtelh tegsh toonii urjver
+// var mul = 1;
+// for(var i = 1; i <= 100; i++){
+//     if(i % 2 == 0){
+//     mul = mul * i;}
+// } console.log(mul)
+
+
+// ----------------1-100 hurtelh sondgoi toonii urjver
+var mul = 1;
+for(var i = 1; i <= 100; i++){
+    if(i % 2 !== 0){
+    mul = mul * i;}
+} console.log(mul)
