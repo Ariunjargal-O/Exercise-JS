145eafe4354cabc29ea3943685e11e4d56471716
24LP2179 24LP2179 <24LP2179@G303-CC0156.local>
2025-02-03 11:59:02 +0800

    plus tragle

---
+++ trange.js
@@ -0,0 +1,32 @@
+//---*-
+//--*-*-
+//--*--*-
+//-*****-
+
+// 1r bagana
+const n = 4;
+for(let i = 0; i < n-1; i++){
+    result = result + "-"
+   
+} result = result + "*-"
+console.log(result)
+
+// goliin 2 mur
+for(let i =0; i <n-2; i++){
+    result = result + "*-"
+    result = result + ""
+}
+
+// 4r suuliin bagana
+for(let i = 0; i < n-1; i++){
+    result = result + "*-"
+   
+} 
+console.log(result)
+
+
+
+
+
+
+
